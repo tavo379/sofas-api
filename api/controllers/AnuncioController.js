33cccdff28d682002934635b13d160c491f7ee84
nestor <nestor.andres.a@gmail.com>
2017-08-02 23:02:44 -0500

    Ajustando anuncions

--- api/controllers/AnuncioController.js
+++ api/controllers/AnuncioController.js
@@ -9,110 +9,46 @@ module.exports = {
       return res.badRequest({err : 'invalid archivos'});
     }
     let images = [];
-
-    archivos.upload({
-      // don't allow the total upload size to exceed ~10MB
-      maxBytes: 10000000
-    },function whenDone(err, uploadedFiles) {
-      if (err) {
-        return res.negotiate(err);
-      }
-
-      // If no files were uploaded, respond with an error.
-      if (uploadedFiles.length === 0){
-        return res.badRequest('No file was uploaded');
-      }
-
-      let anuncioId = req.params.id;
-
-      //validate anuncioId
-      if(!anuncioId){
-        return res.badRequest({err : 'invalid post_id'});
-      }
-
-      //find single post with category
-      let AnuncioUnico = null
-      Anuncios.findOne({
-        unico : '1'
-      }).then(Anuncio => {
-        // Actualizar anuncio
-        Anuncios.update(Anuncio.id, {
-          // Generate a unique URL where the avatar can be downloaded.
-          image1 : require('util').format('%s/assets/images/anunciones/%s', sails.config.appUrl, 1),
-          image1 : require('util').format('%s/assets/images/anunciones/%s', sails.config.appUrl, 2),
-          imageFd1: uploadedFiles[0].fd,
-          imageFd2: uploadedFiles[1].fd,
-          unico: '1'
-        })
-        .exec(function (err){
-          if (err) return res.negotiate(err);
-          return res.ok();
-        });
-      }).catch(err => {
-        // Crear Anuncio
-        let result = Anuncios.create({
-          image1 : require('util').format('%s/assets/images/anunciones/%s', sails.config.appUrl, 1),
-          image1 : require('util').format('%s/assets/images/anunciones/%s', sails.config.appUrl, 2),
-          imageFd1: uploadedFiles[0].fd,
-          imageFd2: uploadedFiles[1].fd,
-          unico: '1'
-        });
-        return res.ok();
+    var path = require('path')
+    req.file('archivos').upload({ dirname: require('path').resolve(sails.config.appPath, 'assets/images/users') }, function (err, uploadedFiles) {
+      if (err) return res.negotiate(err);
+      uploadedFiles.forEach(function(file) {
+        file.fd = normalize(file.fd);
+        images.push('/images/users/' + file.fd.split('/').reverse()[0]);
       });
-
-      // // Save the "fd" and the url where the avatar for a user can be accessed
-      // User.update(req.session.me, {
-
-      //   // Generate a unique URL where the avatar can be downloaded.
-      //   avatarUrl: require('util').format('%s/user/avatar/%s', sails.config.appUrl, req.session.me),
-
-      //   // Grab the first file and use it's `fd` (file descriptor)
-      //   avatarFd: uploadedFiles[0].fd
-      // })
-      // .exec(function (err){
-      //   if (err) return res.negotiate(err);
-      //   return res.ok();
-      // });
+      makeRequest()
+      .then(result => res.ok(result))
+      .catch(err => res.serverError(err));
     });
 
+      // create async method makeRequest
+     const makeRequest = async () =>{
+       try {
+         const anuncios = await Anuncios.find();
+         let anuncioId = false;
+         if (anuncios.length > 0) {
+          anuncioId = anuncios[0].id;
+         }
+         console.log(' ==> anuncioId : ' + anuncioId)
+         if (anuncioId) {
+          const an = await Anuncios.update(anuncioId, {
+            image1: { type: 'string' },
+            image2: { type: 'string' },
+          });
+          //return anuncios and category
+          return an;
+         }
 
-    // var path = require('path')
-    // req.file('archivos').upload({ dirname: require('path').resolve(sails.config.appPath, 'assets/images/users') }, function (err, uploadedFiles) {
-    //   if (err) return res.negotiate(err);
-    //   uploadedFiles.forEach(function(file) {
-    //     file.fd = normalize(file.fd);
-    //     images.push('/images/users/' + file.fd.split('/').reverse()[0]);
-    //   });
-    //   makeRequest()
-    //   .then(result => res.ok(result))
-    //   .catch(err => res.serverError(err));
-    // });
+        const an = await Anuncios.create({
+          images: images,
 
-    //   // create async method makeRequest
-    //  const makeRequest = async () =>{
-    //    try {
-    //      const anuncios = await Anuncios.find();
-    //      let anuncioId = false;
-    //      if (anuncios.length > 0) {
-    //       anuncioId = anuncios[0].id;
-    //      }
-    //      if (anuncioId) {
-    //       const an = await Anuncios.update(anuncioId, {
-    //         images: images,
-    //       });
-    //       //return anuncios and category
-    //       return an;
-    //      }
-
-    //     const an = await Anuncios.create({
-    //       images: images,
-    //     });
-    //     //return anuncios and category
-    //     return an;
-    //    } catch (err){
-    //      throw err;
-    //    }
-    //  };
+        });
+        //return anuncios and category
+        return an;
+       } catch (err){
+         throw err;
+       }
+     };
   },
 
   findAll: function (req, res) {
--- api/models/Anuncios.js
+++ api/models/Anuncios.js
@@ -3,8 +3,6 @@ module.exports = {
   attributes: {
     image1: { type: 'string' },
     image2: { type: 'string' },
-    imageFd1: { type: 'string' },
-    imageFd2: { type: 'string' },
     unico: { type: 'string' }
   }
 };
